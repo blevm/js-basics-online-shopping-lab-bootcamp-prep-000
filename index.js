7153f887941b0e587d8d2e7e647f611791beaf22
Michelle B <mjblevins7@gmail.com>
2017-12-12 03:47:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,46 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  const itemPrice = Math.floor(Math.random() * 100) + 1;
+
+  cart.push({ [item]: itemPrice });
+
+  console.log(`${item} has been added to your cart.`);
+
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  const cartLength = cart.length;
+
+  if (!cartLength) {
+    return console.log("Your shopping cart is empty.");
+  }
+
+  let itemsAndPrices = [];
+
+  for (let cartIndex = 0; cartIndex < cartLength; cartIndex++) {
+    let eachItem = cart[cartIndex];
+    let itemName = Object.keys(eachItem)[0];
+    let itemPrice = eachItem[itemName];
+
+    itemsAndPrices.push(`${itemName} at \$${itemPrice}`);
 }
+
+    switch (itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+        itemsAndPrices[cartLength-1] = "and ".concat(itemsAndPrices[cartLength-1]);
+        itemsAndPrices = itemsAndPrices.join(", ");
+    }
+
+    console.log(`In your cart, you have ${itemsAndPrices}.`);
+}
+
 
 function total() {
   // write your code here
